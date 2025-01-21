09a3f23ebe3aca542677d15740b1aacbe0496b7e
MuhammadIbrahim14 <foraptech080@gmail.com>
2025-01-21 17:01:53 +0500

    #new1

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::post('/update/{update_id}', [stdData::class, 'update'])->name('update');
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
